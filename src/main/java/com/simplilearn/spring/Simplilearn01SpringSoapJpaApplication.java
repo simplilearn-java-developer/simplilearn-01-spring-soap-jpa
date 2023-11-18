99d50e246cec444617d02fa427676e3eca2062da
Carlos Zegarra <cgzegarra@gmail.com>
2023-11-18 12:12:48 -0500

    first commit

---
+++ src/main/java/com/simplilearn/spring/Simplilearn01SpringSoapJpaApplication.java
@@ -0,0 +1,13 @@
+package com.simplilearn.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Simplilearn01SpringSoapJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Simplilearn01SpringSoapJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/simplilearn/spring/Simplilearn01SpringSoapJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simplilearn.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Simplilearn01SpringSoapJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
